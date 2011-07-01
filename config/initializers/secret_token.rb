4dfbddbccd0cc0fc4d6febfbfa12edc5b4260ad7
Landers <christina@pulsd.com>
2011-07-01 16:53:49 -0700

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portfolio::Application.config.secret_token = 'd510418ee8bf687c42d37e0535ea4d61b1c8d3bb9ddb5abdb4c4fd86010e818f24a6a4fc21e915a36f81ae0bfba9493ee867a84e55bcd2218d1e836266753df2'
