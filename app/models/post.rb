972a7fe725e368db0015225b89527088a3ba7f0a
Landers <christina@pulsd.com>
2011-07-07 11:59:46 -0700

    Paperclip working in development, try in production. - added image columnst to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,6 @@ class Post < ActiveRecord::Base
                     :length => { :minimum => 5 }
                     
   has_many :comments, :dependent => :destroy
+  
+  has_attached_file :photo, :styles => { :small => "150x150>" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110706170744) do
+ActiveRecord::Schema.define(:version => 20110707184702) do
 
   create_table "comments", :force => true do |t|
     t.string   "commenter"
@@ -26,7 +26,10 @@ ActiveRecord::Schema.define(:version => 20110706170744) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "image"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
 end
