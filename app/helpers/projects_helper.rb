2b41c4108c3cbce4df5a64074c7af2694bc6f013
Landers <christina@pulsd.com>
2011-07-11 20:45:09 -0700

    Navigation arrows - added navigation arrows - added favicon

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,23 @@
 module ProjectsHelper
+  
+  def next_project_path
+    if @project == Project.last
+      proj_id = 1
+    else
+      proj_id = @project.id
+      proj_id += 1
+    end
+    return "/projects/#{proj_id}"
+  end
+  
+  def prev_project_path
+    if @project == Project.first
+      proj_id = Project.last.id
+    else
+      proj_id = @project.id
+      proj_id -= 1
+    end
+    return "/projects/#{proj_id}"
+  end
+  
 end
